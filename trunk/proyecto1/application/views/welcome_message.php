9c5e61f3d3ad1d705cbcde0b9f21faef5eca713e
wfpantoja <wfpantoja@c8746e20-c2d8-61ac-f751-2c51c4f57c48>
2014-02-25 02:56:47 +0000

    Creacion del Proyecto, subida inicial

---
+++ trunk/proyecto1/application/views/welcome_message.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Sistema de Gestión de Producción Intelectual de Egresados del Departamento de Sistemas</title>
+
+</head>
+<body>
+
+<div id="container">
+	<h1>Sistema de Gestión de Producción Intelectual de Egresados del Departamento de Sistemas</h1>
+
+	<div id="body">
+            <p>Aqui desarrollaremos la aplicacion de gestion.</p>
+	</div>
+</div>
+
+</body>
+</html>
