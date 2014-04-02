715e6686b25bb6fc5066886671ce621e4996f24a
wfpantoja <wfpantoja@c8746e20-c2d8-61ac-f751-2c51c4f57c48>
2014-04-02 05:52:32 +0000

    integracion de crud realizado por guillermo e implementacion de acceso al sistema para administrador, envio de contraseñas a los usuarios registrados en la base de datos.

--- proyecto1/application/models/dao_model.php
+++ proyecto1/application/models/dao_model.php
@@ -11,15 +11,29 @@ class DAO_model extends CI_Model {
 
     function conectar() {
         $this->load->database();
-        echo 'conectado';
         return true;
     }
 
     function registro_docente() {
-        
+
         $sql = "SELECT * FROM some_table WHERE id = ? AND status = ? AND author = ?";
 
         $this->db->query($sql, array(3, 'live', 'Rick'));
+    }
+
+    function get_contrasena_usuario($email) {
+        if ($this->conectar()) {
+            $query = $this->db->query('
+                    SELECT USU_CONTRASENA AS password 
+                    FROM usuario
+                    WHERE USU_EMAIL = ?'
+                    , array($email));
+            if ($query->num_rows() > 0) {
+                $row = $query->row(1);
+                return $row->password;
+            }
+        }
+        return FALSE;
     }
 
 }
