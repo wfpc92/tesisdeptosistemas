a9387dab5d025ca300ebf6e19798405ae02eff2f
guille.dulcey <guille.dulcey@c8746e20-c2d8-61ac-f751-2c51c4f57c48>
2014-04-07 15:45:27 +0000

    Añadi: -vistas: monografia, producciones -modelo: produccion Modifique: -controlador: docente (para que cargue una vista de redireccion a los CRUDS y el CRUD de monografias)

--- trunk/proyecto1/application/controllers/docente.php
+++ trunk/proyecto1/application/controllers/docente.php
@@ -4,10 +4,39 @@ if (!defined('BASEPATH'))
     exit('No direct script access allowed');
 
 class Docente extends CI_Controller {
-
+    
+    function __construct() {
+        parent::__construct();
+        /* Standard Libraries of codeigniter are required */
+		$this->load->database();
+        $this->load->helper('url');
+        /* ------------------ */ 
+ 
+        $this->load->library('grocery_CRUD');
+        $this->load->model('produccion_model');
+    }
     
     public function index() {
         $this->load->view('docente/home');
     }
-
+    
+    public function producciones(){
+        $this->_producciones_output();
+    }
+    
+    public function monografia(){
+        $produccion = new Produccion_model();
+        
+        $output = $produccion->gestion_monografia();
+        
+        $this->_monografia_output($output);
+    }
+    
+    public function _producciones_output() {
+        $this->load->view('docente/producciones.php');
+    }
+    
+    public function _monografia_output($output = null) {
+        $this->load->view('docente/monografia.php', $output);
+    }
 }
---
+++ trunk/proyecto1/application/views/docente/producciones.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+ 
+<style type='text/css'>
+body
+{
+    font-family: Arial;
+    font-size: 14px;
+}
+a {
+    color: blue;
+    text-decoration: none;
+    font-size: 14px;
+}
+a:hover
+{
+    text-decoration: underline;
+}
+</style>
+</head>
+<body>
+<!-- Beginning header -->
+    <div>
+        
+ 
+    </div>
+<!-- End of header-->
+    <div style='height:20px;'></div>
+    <div>
+        <a href='<?php echo site_url('docente/articulo')?>'>Articulo</a> <br> 
+        <a href='<?php echo site_url('docente/monografia')?>'>Monografia</a> <br>
+        <a href='<?php echo site_url('docente/reporte_tecnico')?>'>Reporte Tecnico</a>         
+    </div>
+<!-- Beginning footer -->
+<div>Footer</div>
+<!-- End of Footer -->
+</body>
+</html>
