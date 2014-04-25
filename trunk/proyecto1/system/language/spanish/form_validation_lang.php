8f2a671960ea6280aef959bb485c9b84130f525e
guille.dulcey <guille.dulcey@c8746e20-c2d8-61ac-f751-2c51c4f57c48>
2014-04-25 13:43:32 +0000

    Se modifico el crud de usuarios para que estuviera en español asi como que las validaciones de correo y campos requeridos se presenten correctamente

---
+++ trunk/proyecto1/system/language/spanish/form_validation_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+$lang['required']			= "El campo %s es obligatorio.";
+$lang['valid_email']		= "El campo %s debe contener una dirección de email válida.";
+
+?>
