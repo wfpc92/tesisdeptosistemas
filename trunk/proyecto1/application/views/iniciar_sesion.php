b5e0044d4680289106fe38404a23a2ba8f5782e3
wfpantoja <wfpantoja@c8746e20-c2d8-61ac-f751-2c51c4f57c48>
2014-05-20 17:40:25 +0000

    Integracion de vistas de detalle de produccion, enviar mensaje de contacto a autor.

--- trunk/proyecto1/application/views/iniciar_sesion.php
+++ trunk/proyecto1/application/views/iniciar_sesion.php
@@ -1,10 +1,11 @@
 <div id="inicioSesion">
     <?php
-    if ($this->tank_auth->is_logged_in()) { 
+    if ($this->tank_auth->is_logged_in()) {
         ?>
-    <span class="bienvenido">Bienvenido</span>
-    <?php
-        echo anchor(site_url('usuario/logout'),'Cerrar Sesión').anchor(site_url('usuario/editar_datos'),$nombre);
+        <span class="bienvenido">Bienvenido</span>
+        <?php
+        $nombre = $this->session->userdata("nombre");
+        echo anchor(site_url('usuario/logout'), 'Cerrar Sesión') . anchor(site_url('usuario/editar_datos'), $nombre);
     } else {
         echo anchor(site_url('usuario/login'), 'Iniciar Sesión');
     }
--- trunk/proyecto1/application/views/produccion/item_busqueda.php
+++ trunk/proyecto1/application/views/produccion/item_busqueda.php
@@ -1,19 +1,23 @@
 <div id="itemBusqueda">
     <?php
     if (isset($PROD_CODIGO)):
-        echo anchor(site_url('produccion/ver_detalle/' . $PROD_CODIGO), $PROD_TITULO)
+        echo anchor(site_url('produccion/ver_detalle/' . $PROD_CODIGO), $PROD_TITULO);
         ?>
         <br />
 
-    <ul>
-        <li> <span>Fecha Publicación: </span> <?php echo $PROD_FECHA_PUBLICACION ?> </li>
-        <li class="resumen"> <?php
-            $resumenDetallado = $PROD_RESUMEN;
-            $resumenBusqueda = substr($resumenDetallado, 0, 250) . "...";
+        <ul>
+            <li> <span>Fecha Publicación: </span> <?php echo $PROD_FECHA_PUBLICACION ?> </li>
+            <li class="resumen"> <?php
+                $resumenDetallado = $PROD_RESUMEN;
+                $resumenBusqueda = substr($resumenDetallado, 0, 250) . "...";
 
-            echo $resumenBusqueda;?>
-        </li>
-    </ul>
+                echo $resumenBusqueda;
+                ?>
+            </li>
+        </ul>
+        <?php
+    endif;
+    ?>
 </div>
 
 
