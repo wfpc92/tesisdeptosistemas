d21e9b3a6cd80fc10d4b48f02ca71d6ace57cbdc
guille.dulcey <guille.dulcey@c8746e20-c2d8-61ac-f751-2c51c4f57c48>
2014-05-27 17:50:17 +0000

    Nuevo Menu corregido con nuevas opciones

--- proyecto1/application/views/enlaces.php
+++ proyecto1/application/views/enlaces.php
@@ -3,7 +3,10 @@
     echo 'Encuentre aquí:' . br();
     ?>
     <a href="http://www.unicauca.edu.co">Unicauca</a><br />
-    <a href=http://www.unicauca.edu.co/sistemas>Facultad de Sistemas</a><br />    
+    <a href="http://www.unicauca.edu.co/sistemas">Facultad de Sistemas</a><br />    
+    <a href="http://www.unicauca.edu.co/sistemas/docentes">Docentes</a><br />
+    <a href="http://www.unicauca.edu.co/sistemas/investigaci%C3%B3n">Grupos de Investigación</a><br />
+    <a href="http://biblio.unicauca.edu.co/">Biblioteca Virtual</a><br />
     <a href="http://simca.unicauca.edu.co">SIMCA</a><br />
     <a href="http://www.unicauca.edu.co/sistemas/contacto">Escribale al Jefe</a><br />
 </div>
