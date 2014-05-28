679131833abb929838ed830f12bd8582cf408194
karenj2105@gmail.com <karenj2105@gmail.com@c8746e20-c2d8-61ac-f751-2c51c4f57c48>
2014-05-28 15:43:14 +0000

    estilos menus

--- proyecto1/application/views/docente/home.php
+++ proyecto1/application/views/docente/home.php
@@ -1,15 +1,18 @@
-<div id="menuDocente">    
-    <ul>
+<div id="menuDocente">
+    <div class="menuProducciones">
         <h3>Menú Producciones</h3>
-        <li><?php echo anchor(site_url('docente/monografia'), 'Mis Monografias') ?></li>
-        <li><?php echo anchor(site_url('docente/reporte'), 'Mis Reportes') ?></li>
-        <li><?php echo anchor(site_url('docente/articulo'), 'Mis Articulos') ?></li>
-        
-    </ul>
+        <ul>        
+            <li><?php echo anchor(site_url('docente/monografia'), 'Mis Monografias') ?></li>
+            <li><?php echo anchor(site_url('docente/reporte'), 'Mis Reportes') ?></li>
+            <li><?php echo anchor(site_url('docente/articulo'), 'Mis Articulos') ?></li>        
+        </ul>
+    </div>    
     <br />
-    <ul>
+    <div class="menuEstadisticas">
         <h3>Estadisticas de Publicaciones</h3>
-        <li><?php echo anchor(site_url('docente/estadisticas'), 'Estadisticas de Grupos') ?></li>
-    </ul>    
+        <ul>
+            <li><?php echo anchor(site_url('docente/estadisticas'), 'Estadisticas de Grupos') ?></li>
+        </ul> 
+    </div>   
 </div>
 
--- proyecto1/application/views/enlaces.php
+++ proyecto1/application/views/enlaces.php
@@ -1,12 +1,12 @@
-<div>    
-    <?php
-    echo 'Encuentre aquí:' . br();
-    ?>
-    <a href="http://www.unicauca.edu.co">Unicauca</a><br />
-    <a href="http://www.unicauca.edu.co/sistemas">Facultad de Sistemas</a><br />    
-    <a href="http://www.unicauca.edu.co/sistemas/docentes">Docentes</a><br />
-    <a href="http://www.unicauca.edu.co/sistemas/investigaci%C3%B3n">Grupos de Investigación</a><br />
-    <a href="http://biblio.unicauca.edu.co/">Biblioteca Virtual</a><br />
-    <a href="http://simca.unicauca.edu.co">SIMCA</a><br />
-    <a href="http://www.unicauca.edu.co/sistemas/contacto">Escribale al Jefe</a><br />
+<div id="linkDestacados"> 
+    <h3>Destacados</h3>
+    <ul>        
+        <li><a href="http://www.unicauca.edu.co">Unicauca</a></li>
+        <li class="deptSistemas"><a href="http://www.unicauca.edu.co/sistemas">Departamento de Sistemas</a></li>
+        <li><a href="http://www.unicauca.edu.co/sistemas/docentes">Docentes</a></li>
+        <li class="grupoInves"><a href="http://www.unicauca.edu.co/sistemas/investigaci%C3%B3n">Grupos de Investigación</a></li>
+        <li><a href="http://biblio.unicauca.edu.co/">Biblioteca Virtual</a></li>
+        <li><a href="http://simca.unicauca.edu.co">SIMCA</a></li>
+        <li><a href="http://www.unicauca.edu.co/sistemas/contacto">Contacte al Jefe</a></li>        
+    </ul>
 </div>
