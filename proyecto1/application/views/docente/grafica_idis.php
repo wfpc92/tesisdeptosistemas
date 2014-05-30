efb7a251675d74f6a0242f7e0915d083bec6b28d
guille.dulcey <guille.dulcey@c8746e20-c2d8-61ac-f751-2c51c4f57c48>
2014-05-30 02:40:02 +0000

    Graficas, parcialmente implementadas

---
+++ proyecto1/application/views/docente/grafica_idis.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+  
+   echo $graph->Stroke();
+
+?>
+<!--<img src="grafica_torta_idis.php" alt="" border="0">-->
+<!--<img src="<?php echo $graph->Stroke();?>" alt="" border="0">-->
