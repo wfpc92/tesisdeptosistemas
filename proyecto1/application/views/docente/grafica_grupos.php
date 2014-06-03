51da9f24635b1b677caf92812606bdd4762903f8
guille.dulcey <guille.dulcey@c8746e20-c2d8-61ac-f751-2c51c4f57c48>
2014-06-03 02:16:41 +0000

    Graficas implementadas (muy rusticamente) *se me habia olvidado esta vista

---
+++ proyecto1/application/views/docente/grafica_grupos.php
@@ -0,0 +1,10 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+     //$email = $this->session->userdata('username');
+?>
+<!--<img src="<?php echo '/proyecto1/img/grafica_de_barras'.$email.'.png' ?>" alt="" border="0">-->
+<img src="<?php echo '/proyecto1/img/grafica_de_barras_grupos.png' ?>" alt="" border="0">
