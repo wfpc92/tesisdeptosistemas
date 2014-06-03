2c8fd10117d8b0f05a05f51641c9858223183ba2
guille.dulcey <guille.dulcey@c8746e20-c2d8-61ac-f751-2c51c4f57c48>
2014-06-03 02:16:41 +0000

    Graficas implementadas (muy rusticamente) *se me habia olvidado esta vista

--- trunk/proyecto1/application/views/docente/grafica_idis.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-     $email = $this->session->userdata('username');
-?>
-<img src="<?php echo '/proyecto1/img/grafica_de_barras'.$email.'.png' ?>" alt="" border="0">
