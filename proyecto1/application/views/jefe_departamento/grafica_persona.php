2146eeecb0c19d03ae562d08d6eff6d69918fb99
wfpantoja <wfpantoja@c8746e20-c2d8-61ac-f751-2c51c4f57c48>
2014-06-10 05:03:49 +0000

    Arreglando requisito de estadisticas, se hicieron unos condicionales para controlar las direcciones donde se toman las imagenes y la libreria

--- proyecto1/application/views/jefe_departamento/grafica_persona.php
+++ proyecto1/application/views/jefe_departamento/grafica_persona.php
@@ -1,4 +1,4 @@
 <div id="grafsJefe">
-    <img src="<?php echo '/proyecto1/img/grafica_de_barras_persona.png' ?>" alt="" border="0">
+    <img src="<?php echo base_url('img/grafica_de_barras_persona.png') ?>" alt="" border="0">
     <button class="regresar" onclick="goBack()">Regresar</button>
 </div>
